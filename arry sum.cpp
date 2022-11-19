73bf70eb79c04dfe29a857525db485d41f23f4f2
ArafaAkter <117437951+ArafaAkter@users.noreply.github.com>
2022-11-19 20:48:27 +0600

    Add files via upload

---
+++ arry sum.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n;
+    cin>>n;
+    int a[n],long sum=0;
+
+     for(i=0;i<n;i++)
+     {
+        cin >> a[i];
+        sum = sum + a[i];
+
+     }
+     cout << "sum is:  "<< sum;
+
+
+    return 0;
+}
